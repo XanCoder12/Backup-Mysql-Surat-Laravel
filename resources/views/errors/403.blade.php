a90b7871296b3cff2e01c719baf98aaa4542e2ce
Ye-Shaiyoe <akrommuhammadyusuf@gmail.com>
2026-04-22 08:44:36 +0700

    Melakukan sedikit perubahan di 403 akses di batasi saat ini atau di tolak

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('title', 'Akses Dibatasi Untuk Saat Ini')
+@section('code', '403')
+@section('message', 'Akses Dibatasi')
+@section('description', 'Maaf, Anda tidak memiliki izin untuk mengakses halaman ini. Silakan hubungi administrator jika Anda merasa ini adalah kesalahan.')
