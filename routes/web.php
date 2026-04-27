418bc9380add8cc0a1cc362c3f52d66a39d1e9ed
Ye-Shaiyoe <akrommuhammadyusuf@gmail.com>
2026-04-27 15:55:21 +0700

    Menambahkan routes ke log tabel user manajemen admin , asspirasi tempat mengajukan pertannyaan dan lainnya

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/faq', [UserDashboard::class, 'faq'])->name('user.faq.index');
     Route::get('/statistik', [UserStatistik::class, 'index'])->name('user.statistik.index');
+    Route::get('/notifikasi', [\App\Http\Controllers\User\NotifikasiController::class, 'index'])->name('user.notifikasi.index');
+    Route::get('/aspirasi', [\App\Http\Controllers\User\AspirasiController::class, 'index'])->name('user.aspirasi.index');
+    Route::post('/aspirasi', [\App\Http\Controllers\User\AspirasiController::class, 'store'])->name('user.aspirasi.store');
 
     Route::prefix('surat')->name('user.surat.')->group(function () {
         Route::get('/',          [UserSurat::class, 'index'])->name('index');
@@ -98,30 +101,38 @@ Route::prefix('Admin')->middleware(['auth', 'verified', 'admin'])->name('admin.'
         Route::post('/Template', [TemplateSuratController::class, 'store'])->name('template.store');
         Route::delete('/Template', [TemplateSuratController::class, 'destroy'])->name('template.destroy');
 
-        // Users / Pegawai
-        Route::get('/Users', [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
-        Route::get('/Users/{user}', [\App\Http\Controllers\Admin\UserController::class, 'show'])->name('users.show');
-        Route::delete('/Users/{user}', [\App\Http\Controllers\Admin\UserController::class, 'destroy'])->name('users.destroy');
+        // Settings Group
+        Route::prefix('Settings')->group(function () {
+            // Users / Pegawai
+            Route::get('/Users', [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
+            Route::get('/Users/{user}', [\App\Http\Controllers\Admin\UserController::class, 'show'])->name('users.show');
+            Route::delete('/Users/{user}', [\App\Http\Controllers\Admin\UserController::class, 'destroy'])->name('users.destroy');
+
+            // Log System
+            Route::get('/Logs', [\App\Http\Controllers\Admin\LogController::class, 'index'])->name('logs.index');
+            Route::get('/Logs/download/{file}', [\App\Http\Controllers\Admin\LogController::class, 'download'])->name('logs.download');
+            Route::post('/Logs/clear/{file}', [\App\Http\Controllers\Admin\LogController::class, 'clear'])->name('logs.clear');
+            Route::delete('/Logs/delete/{file}', [\App\Http\Controllers\Admin\LogController::class, 'delete'])->name('logs.delete');
+
+            // Manajemen File Fisik Surat
+            Route::get('/File-Surat', [\App\Http\Controllers\Admin\FileSuratController::class, 'index'])->name('file.index');
+            Route::delete('/File-Surat/{surat}', [\App\Http\Controllers\Admin\FileSuratController::class, 'destroy'])->name('file.destroy');
+            Route::post('/File-Surat/mass-delete', [\App\Http\Controllers\Admin\FileSuratController::class, 'massDelete'])->name('file.massDelete');
+        });
 
         // Notifikasi Admin
         Route::get('/Notifikasi', [\App\Http\Controllers\Admin\NotifikasiController::class, 'index'])->name('notifikasi.index');
+        
+        // Manajemen Aspirasi
+        Route::get('/Aspirasi', [\App\Http\Controllers\Admin\AspirasiController::class, 'index'])->name('aspirasi.index');
+        Route::patch('/Aspirasi/{aspirasi}', [\App\Http\Controllers\Admin\AspirasiController::class, 'update'])->name('aspirasi.update');
+        Route::post('/Aspirasi/{aspirasi}/read', [\App\Http\Controllers\Admin\AspirasiController::class, 'markAsRead'])->name('aspirasi.read');
+
         Route::post('/Notifikasi/read/{id}', [\App\Http\Controllers\Admin\NotifikasiController::class, 'markAsRead'])->name('notifikasi.read');
         Route::post('/Notifikasi/read-all', [\App\Http\Controllers\Admin\NotifikasiController::class, 'markAllAsRead'])->name('notifikasi.readAll');
         Route::delete('/Notifikasi/{id}', [\App\Http\Controllers\Admin\NotifikasiController::class, 'destroy'])->name('notifikasi.delete');
         Route::delete('/Notifikasi', [\App\Http\Controllers\Admin\NotifikasiController::class, 'destroyAll'])->name('notifikasi.deleteAll');
 
-        // Log System
-        Route::get('/Logs', [\App\Http\Controllers\Admin\LogController::class, 'index'])->name('logs.index');
-
-        // Manajemen File Fisik Surat
-        Route::get('/File-Surat', [\App\Http\Controllers\Admin\FileSuratController::class, 'index'])->name('file.index');
-        Route::delete('/File-Surat/{surat}', [\App\Http\Controllers\Admin\FileSuratController::class, 'destroy'])->name('file.destroy');
-        Route::post('/File-Surat/mass-delete', [\App\Http\Controllers\Admin\FileSuratController::class, 'massDelete'])->name('file.massDelete');
-
-        // Logs
-        Route::get('/Logs/download/{file}', [\App\Http\Controllers\Admin\LogController::class, 'download'])->name('logs.download');
-        Route::post('/Logs/clear/{file}', [\App\Http\Controllers\Admin\LogController::class, 'clear'])->name('logs.clear');
-        Route::delete('/Logs/delete/{file}', [\App\Http\Controllers\Admin\LogController::class, 'delete'])->name('logs.delete');
     });
 });
 
